4cd0c1091b5f4fe7e545ffb9eab63896a98acc5b
Jonathan <jonathancamiling@gmail.com>
2020-12-08 13:28:44 -0800

    added user ability to change difficulty, stored in local storage and corresponding with updateTime based on the difficulty selected

--- script.js
+++ script.js
@@ -44,6 +44,12 @@ const words = [
   //Init time
   let time = 10;
 
+  //Set difficulty ot value in local storage or medium
+  let difficulty = localStorage.getItem('difficulty') !== null ? localStorage.getItem('difficulty') : 'medium';
+
+  //Set difficulty select value
+  difficultySelect.value = localStorage.getItem('difficulty') !== null ? localStorage.getItem('difficulty') : 'medium';
+
   //Focus on text on start
   text.focus(); 
 
@@ -93,6 +99,8 @@ endgameEl.style.display = 'flex';
 addWordToDOM(); 
 
 //Event listeners
+
+//Typing
 text.addEventListener('input', e => {
     const insertedText = e.target.value;
     if (insertedText === randomWord) {
@@ -101,8 +109,23 @@ text.addEventListener('input', e => {
 
         //Clear
         e.target.value = '';
-        time += 5; 
+        if(difficulty === 'hard'){
+            time +=2;
+        } else if (difficulty === 'medium'){
+            time +=3.5;
+        } else {
+           time += 5;  
+        }
         updateTime(); 
     }
 }); 
+
+//Settings btn click
+settingsBtn.addEventListener('click', () => settings.classList.toggle('hide'));
+
+//Settings select
+settingsForm.addEventListener('change', e => {
+    difficulty = e.target.value; 
+    localStorage.setItem('difficulty', difficulty);
+})
   
